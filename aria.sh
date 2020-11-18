4c4818642a25715aa941221c83d591f60b5abfe5
yamraaj007 <69627019+crazy-racer@users.noreply.github.com>
2020-11-18 21:51:20 +0530

    Update aria.sh

--- aria.sh
+++ aria.sh
@@ -4,4 +4,5 @@ aria2c --enable-rpc --rpc-listen-all=false --rpc-listen-port 6800 \
   --max-connection-per-server=16 --rpc-max-request-size=1024M \
   --seed-time=0.01 --min-split-size=10M --follow-torrent=mem --split=10 \
    --daemon=true --allow-overwrite=true --max-overall-download-limit=$MAX_DOWNLOAD_SPEED \
-   --max-overall-upload-limit=1K --max-concurrent-downloads=$MAX_CONCURRENT_DOWNLOADS --peer-id-prefix=-TR2940- --user-agent=Transmission 2.94
+   --max-overall-upload-limit=1K --max-concurrent-downloads=$MAX_CONCURRENT_DOWNLOADS --peer-id-prefix=-TR2940- --user-agent=Transmission 2.94 \
+   --peer-agent=qBittorrent/4.2.5 --peer-id-prefix=-qB4250-
